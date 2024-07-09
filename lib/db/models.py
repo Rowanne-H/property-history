6fa45b770af4cc51a8db23b84b9cef374d32e03d
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-09 21:37:28 +1000

    create Property model

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 engine = create_engine('sqlite:///property_history.db')
 
@@ -10,6 +11,8 @@ class Owner(Base):
     __tablename__ = 'owners'
 
     id = Column(Integer(), primary_key=True)
+    
+    properties = relationship('Property', backref='owner')
 
     def __repr__(self):
         return f'Owner(id={self.id})'
@@ -19,6 +22,18 @@ class Agent(Base):
 
     id = Column(Integer(), primary_key=True)
 
+    properties = relationship('Property', backref='agent')
+
     def __repr__(self):
         return f'Agent(id={self.id})'
+    
+class Property(Base):
+    __tablename__ = 'properties'
+
+    id = Column(Integer(), primary_key=True)
+    owner_id = Column(Integer(), ForeignKey('owners.id'))
+    agent_id = Column(Integer(), ForeignKey('agents.id'))
+
+    def __repr__(self):
+        return f'Property(id={self.id})'
 
