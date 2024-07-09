05cc5e9a42ab06224f376b03c8d1917a6c3d566b
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-09 23:15:18 +1000

    create a function to print out owner and agent id when a property id is given

--- lib/cli.py
+++ lib/cli.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///db/property_history.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+from db.models import Owner, Agent, Property
+
 if __name__ == '__main__':
     property_id = input("Please enter property id: ")
-    print(property_id)
+
+    property = session.query(Property).filter(Property.id == property_id)[0] 
+
+    print(f'This property belongs to owner with id {property.owner_id} and is managed by agent with id {property.agent_id}')
