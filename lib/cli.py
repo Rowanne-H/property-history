57fb0fbedb4962e8a552fe144d7902e8c3571194
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-09 23:50:46 +1000

    create function to validate input and print out owner and agent id when input is valid

--- lib/cli.py
+++ lib/cli.py
@@ -11,7 +11,23 @@ from db.models import Owner, Agent, Property
 
 if __name__ == '__main__':
     property_id = input("Please enter property id: ")
+    
+    while property_id:
+        if property_id.isdigit():
+            if 1 <= int(property_id) <= 500:
+                property = session.query(Property).filter(Property.id == property_id)[0] 
+                print(f'This property belongs to owner with id {property.owner_id} and is managed by agent with id {property.agent_id}')
+                property_id = input("Please enter property id: ")
+            else:
+                property_id = input("Please enter an integer between 1 and 500: ")
+        else:
+            property_id = input("Please enter an integer: ")
 
-    property = session.query(Property).filter(Property.id == property_id)[0] 
+        
+        
 
-    print(f'This property belongs to owner with id {property.owner_id} and is managed by agent with id {property.agent_id}')
+    
+    
+        
+
+    
